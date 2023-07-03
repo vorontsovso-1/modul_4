0c39bf1e95f67e0f3eac4147c777f9cdca5f51f4
Sergey <137311616+vorontsovso-1@users.noreply.github.com>
2023-07-03 10:34:03 +0300

    Create 2.4.py

---
+++ 2.4.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Заголовок 1 уровня</h1>
+<h2>Заголовок 2 уровня</h2>
+<h3>Заголовок 3 уровня</h3>
+<h4>Заголовок 4 уровня</h4>
+<h5>Заголовок 5 уровня</h5>
+<h6>Заголовок 6 уровня</h6>
+<p class="paragraph-1">Параграф 1</p>
+<p class="paragraph-2">Параграф 2</p>
+<p class="paragraph-2">Параграф 3</p>
+
+<div>
+    <span>Hello</span><br>
+    <span>World</span>
+</div>
+ <img src="https://fotooboimoi.ru/upload/iblock/361/36143f87f17bd18614ba1ffaa2fb650d.jpg">
+
+</body>
+</html>
