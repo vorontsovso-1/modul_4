193bfd14839fca717d17675d7f7a965b6853596f
Sergey <137311616+vorontsovso-1@users.noreply.github.com>
2023-07-03 10:37:00 +0300

    Create 2.5.py

---
+++ 2.5.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+font-style: italic;
+text-align: center;
+font-size: 24px;
+color: green;
+text-shadow: 2px 2px 2px green;
+}
+</style>
+</head>
+<body>
+<p>Текст с наклонным шрифтом, отцентрированный, с тенью зеленого цвета.</p>
+</body>
+</html>
